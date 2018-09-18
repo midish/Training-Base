d4f99444dc84784769fa0a6c8f10e1f04e689abc
midish <zshykov@gmail.com>
2018-09-19 00:31:09 +0300

    Добавил комментарии

--- sex2string.c
+++ sex2string.c
@@ -22,23 +22,29 @@ char     **sex2string(char **strings)
     int i = 0;
     int x = 0;
     int y = 0;
-
+    
+    // Выделяю память под matrix[x]
     matrix = (char **) malloc(3 * sizeof(char *));
+    // Выделяю память под matrix[x][y]
     while (i < 3)
     {
         matrix[i] = (char *) malloc(3 * sizeof(char));
         i++;
     }
+    // Присваиваю значение хранящееся в массиве из мэйна, массиву matrix
+    // Сначала для первого уровня matrix[x]
     while (x < 3)
     {
         matrix[x] = strings[x];
         x++;
+        // Затем для второго уровня matrix[x][y]
         while (y < 3)
         {
             matrix[x][y] = strings[x][y];
             y++;
         }
     }
+    // Возвращаю указатель на массив обратно в main
     return (matrix);
 }
 
@@ -48,7 +54,7 @@ int     main(void)
     int i = 0;
     char *sexystrings[3] = { "123", "456", "789"};
     char **sudokumatrix; // Двухмерный массив будет хранит ЦИФРЫ в виде [1...9][1...9]
-
+    // Печатаю в консоль каждую строку в sudokumatrix[i]
     while (i < 3)
     {
         sudokumatrix = sex2string(sexystrings);
