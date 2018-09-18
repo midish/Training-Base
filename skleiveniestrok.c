6c66fecd9e8adbbb3c93f8f28f0822dd81a4681f
midish <zshykov@gmail.com>
2018-09-18 13:57:18 +0300

    Stroki have sex and make new string

---
+++ skleiveniestrok.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int     ft_strlen(char *str)
+{
+    int i = 0;
+
+    while (str[i])
+    {
+        i++;
+    }
+    return (i);
+}
+
+int     main(void)
+{
+    char *a = "Hello ";
+    char *b = "World!";
+    char *c;
+    int i = 0;
+    int j = 0;
+
+    c = (char *)malloc(sizeof(char) + ft_strlen(a) + ft_strlen(b) + 1);
+    while (b[j] != '\0')
+    {
+        if (a[i] != '\0')
+        {
+            c[i] = a[i];
+        }
+        else if (a[i] == '\0')
+        {
+            while (b[j] != '\0')
+            {
+                c[i] = b[j];
+                j++;
+                i++;
+            }
+        }
+        i++;
+    }
+    c[i] = '\0';
+    printf("%s\n", c);
+    printf("%d\n", ft_strlen(c));
+    return (0);
+}
